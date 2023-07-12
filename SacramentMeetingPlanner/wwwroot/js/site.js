fe5d820959b072cee34ceecd7e7dfbdf969ec9d0
Kylar Sorensen <kylar.sorensen@gmail.com>
2023-07-12 16:58:53 -0700

    added quality of life improvements

--- SacramentMeetingPlanner/wwwroot/js/site.js
+++ SacramentMeetingPlanner/wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+var textInputs = document.querySelectorAll('input[type="text"]');
+textInputs.forEach(function (input) {
+    input.addEventListener('click', function () {
+        this.select(); // Automatically select the text inside the input field
+    });
+});
